7e17c237527884fefbb11a8e63aee491b3cbe53c
jaro <jaro@jaros-MacBook-Pro.local>
2026-02-12 18:55:14 +0100

    Fix missing Dict import in database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import sqlite3
 from contextlib import contextmanager
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 from loguru import logger
 
